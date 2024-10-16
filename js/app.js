37cccc4e294e3a575f84dbde44c62a4db63ac03e
TyDaDev <tyrone.cartwright@yahoo.com>
2024-10-16 03:06:59 -0400

    wip: added script to index.html && got a working nav to display with active state

--- js/app.js
+++ js/app.js
@@ -18,110 +18,113 @@
  * Great to have comments before crucial code sections within the procedure.
 */
 
+document.addEventListener('DOMContentLoaded', function () { 
 
-/**
- * Define Global Variables
- * 
-*/
-const navBar = document.querySelector('.navbar__menu');
-const navList = document.getElementById('navbar__list');
-const sections = document.querySelectorAll('section');
-const footer = document.querySelector('.page__footer');
-const header = document.querySelector('.page__header');
-
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// Create a function to build the nav
-function buildNav() {
-  for (let section of sections) {
+  /**
+   * Define Global Variables
+   * 
+  */
+  const navBar = document.querySelector('.navbar__menu');
+  const navList = document.getElementById('navbar__list');
+  const sections = document.querySelectorAll('section');
+  const header = document.querySelector('.page__header');
+  
+  
+  /**
+   * End Global Variables
+   * Start Helper Functions
+   * 
+  */
+  
+  
+  
+  /**
+   * End Helper Functions
+   * Begin Main Functions
+   * 
+  */
+  
+  // Create a function to build the nav
+  function buildNav() {
+    sections.forEach((section) => {
       // Create a list item
-    const navItem = document.createElement('li');
-    // Create a link
-    navItem.innerHTML = `<a href="#${section.id}" class="menu__link">${section.dataset.nav}</a>`;
-// Append the li to the ul
-    navList.appendChild(navItem);
-
-    // Scroll to section on link click
-    navItem.addEventListener('click', function () {
-      event.preventDefault();
-      section.scrollIntoView({behavior: 'smooth'});
-    });        
+      const navItem = document.createElement('li');
+      // Create a link
+      navItem.innerHTML = `<a href="#${section.id}" class="menu__link">${section.dataset.nav}</a>`;
+      // Append the li to the ul
+      navList.appendChild(navItem);
+  
+      // Scroll to section on link click
+      navItem.addEventListener('click', function (event) {
+        event.preventDefault();
+        section.scrollIntoView({behavior: 'smooth'});
+      });        
+    });
+    
+  } 
+  
+  
+  
+  // Add class 'active' to section when near top of viewport
+  function setActiveSection() { 
+    // Loop through the sections
+    sections.forEach(section => {
+      // Get the position of the section
+      const sectionPosition = section.getBoundingClientRect();
+      // Check if the section is in the viewport
+      if (sectionPosition.top >= 0 && sectionPosition.top <= window.innerHeight / 2) {
+        // Add the active class to the section
+        section.classList.add('active');
+      } else {
+        // Remove the active class from the section
+        section.classList.remove('active');
+      }
+    });
+  } 
+  
+  
+  // Create a function to scrollTo anchor ID using scrollTO event
+  
+  
+  // Create a function to show/hide navigation menu
+  function showHideNav() { 
+    let timer = null;
+    window.addEventListener('scroll', function () {
+      // Set the active section to active
+      setActiveSection();
+      // Show the navigation menu
+      header.style.display = 'block';
+      // Hide the navigation menu after 3 seconds
+      if (timer){
+        clearTimeout(timer);
+      }
+      timer = setTimeout(() => {
+        header.style.display = 'none';
+      }, 3000)
+    });
   }
-  navBar.appendChild(navList);
-} 
-
-
-
-// Add class 'active' to section when near top of viewport
-function setActiveSection() { 
-  // Select all sections
-  for (let section of sections) {
-    // Get the position of the section
-    const sectionPosition = section.getBoundingClientRect();
-    // Add class 'active' to the section when it is near the top of viewport
-    if (sectionPosition.top >= 0 && sectionPosition.bottom < window.innerHeight / 2) {
-      section.classList.add('active');
-    } else {
-      section.classList.remove('active');
-    }
+  
+  
+  /**
+   * End Main Functions
+   * Begin Events
+   * 
+  */
+  
+  if (navBar && navList && sections.length > 0 && header) {
+    buildNav();
+    showHideNav();
+  } else {
+    console.error('One or more elements not found in the DOM');
   }
-}
-
-
-
-// Create a function to scrollTo anchor ID using scrollTO event
-
-
-// Create a function to show/hide navigation menu
-function showHideNav() { 
-  let timer = null;
-  window.addEventListener('scroll', function () {
-    // Set the active section to active
-    setActiveSection();
-    header.style.display = 'block';
-    // Show the navigation menu
-    if (timer){
-      clearTimeout(timer);
-    }
-    timer = setTimeout(() => {
-      header.style.display = 'none';
-    }, 3000)
-  });
-}
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-if (navBar && navList && sections.length > 0 && header) {
-  buildNav();
-  showHideNav();
-} else {
-  console.error('One or more elements not found in the DOM');
-}
-
-// Build menu
-
-
-
-// Scroll to section on link click
-
-
-
-// Set sections as active
+  
+  // Build menu
+  
+  
+  
+  // Scroll to section on link click
+  
+  
+  
+  // Set sections as active
+});
